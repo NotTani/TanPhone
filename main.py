15b2aa59be09b4bad7f1ffbe512815d3229cb7f8
Tani N-K <tnevins-klein@thegraysonschool.org>
2021-11-08 14:08:25 -0500

    we're using Quart and venv now

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
+from quart import Quart, render_template
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+async def index():
+    return await render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
